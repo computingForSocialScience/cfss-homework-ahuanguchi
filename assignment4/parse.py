b72f5bd3256e10b79583e5104cb1763292901bb4
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-01-29 21:31:28 -0600

    Parts 2 to 4

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,6 +1,8 @@
 import csv
 import sys
 
+import pylab
+
 def readCSV(filename):
     '''Reads the CSV file `filename` and returns a list
     with as many items as the CSV has rows. Each list item 
@@ -14,3 +16,71 @@ def readCSV(filename):
 
 
 ### enter your code below
+
+def get_avg_latlng(csv_lines):
+    all_lat = [float(line[128]) for line in csv_lines]
+    all_long = [float(line[129]) for line in csv_lines]
+    num_lines = len(csv_lines)
+    avg_lat = sum(all_lat) / num_lines
+    avg_long = sum(all_long) / num_lines
+    print('Average latitude, longitude: %s, %s' % (avg_lat, avg_long))
+
+def zip_code_barchart(csv_lines):
+    zipcode_columns = [28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126]
+    zipcodes = []
+    for line in csv_lines:
+        for column in zipcode_columns:
+            zip = line[column]
+            if zip != '':
+                zip = zip[:5]
+                zipcodes.append(zip)
+    zip_set = sorted(set(zipcodes))    # sorted list of unique zip codes
+    zip_counts = [zipcodes.count(zip) for zip in zip_set]
+    index = range(1, len(zip_set) + 1)
+    fig = pylab.figure()
+    ax = fig.add_subplot(111)
+    ax.bar(index, zip_counts)
+    ax.set_title('Counts of Contractor Zip Codes')
+    ax.set_xticks(index)
+    ax.set_xticklabels(zip_set, rotation=45)
+    pylab.savefig('barchart.jpg')
+
+def command_line():
+    """Use 'latlong' or 'hist' after 'parse.py' to call a function."""
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        print("Use 'latlong' or 'hist' after 'parse.py' to call a function.")
+        return
+    permits = readCSV('permits_hydepark.csv')
+    if command == 'latlong':
+        get_avg_latlng(permits)
+    elif command == 'hist':
+        zip_code_barchart(permits)
+        print('Bar chart created!')
+    else:
+        print("Use 'latlong' or 'hist' after 'parse.py' to call a function.")
+
+def _find_zipcode_headers():
+    '''Find the indices of zipcode headers and print them to a file as a list.'''
+    original = readCSV('permits.csv')
+    headers = original[0]
+    zipcode_columns = []
+    zipcode_column_names = []
+    for i in xrange(len(headers)):
+        if 'ZIPCODE' in headers[i]:
+            zipcode_columns.append(i)
+            zipcode_column_names.append(headers[i])
+    with open('header_indices.txt', 'w') as file:
+        file.write(str(zipcode_columns) + '\n\n')
+        file.write(str(zipcode_column_names))
+
+def _test():
+    permits = readCSV('permits_hydepark.csv')
+    get_avg_latlng(permits)
+    zip_code_barchart(permits)
+
+if __name__ == '__main__':
+    # _find_zipcode_headers()
+    # _test()
+    command_line()
