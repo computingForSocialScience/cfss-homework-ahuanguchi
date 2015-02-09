d690346e5deb08ec7f1aaa191592b1736bc3f87e
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-02-09 13:13:03 -0600

    Began working on assignment

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -1,15 +1,33 @@
 import requests
 from datetime import datetime
 
+# https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj
+
 def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
     returns a list of album IDs in a list
     """
-    pass
+    url = 'https://api.spotify.com/v1/artists/' + artist_id + '/albums?market=US&album_type=album'
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+    dct = req.json()
+    assert dct.get('items'), 'No albums found.'
+    return [album['id'] for album in dct['items']]
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
     and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
     """
-    pass
+    url = 'https://api.spotify.com/v1/albums/' + album_id
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+    dct = req.json()
+    album_info = {}
+    assert dct.get('name'), 'Album not found'
+    album_info['artist_id'] = dct['artists'][0]['id']
+    album_info['album_id'] = album_id
+    album_info['name'] = dct['name']
+    album_info['year'] = dct['release_date'][:4]
+    album_info['popularity'] = dct['popularity']
+    return album_info
 
--- Assignment5/fetchArtist.py
+++ Assignment5/fetchArtist.py
@@ -2,16 +2,35 @@ import sys
 import requests
 import csv
 
+
 def fetchArtistId(name):
     """Using the Spotify API search method, take a string that is the artist's name, 
     and return a Spotify artist ID.
     """
-    pass
+    name = name.lower().replace(' ', '%20')
+    url = 'https://api.spotify.com/v1/search?q=' + name + '&type=artist'
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+    dct = req.json()
+    assert dct.get('artists').get('items'), 'Artist not found.'
+    return dct['artists']['items'][0]['id']
+
 
 def fetchArtistInfo(artist_id):
     """Using the Spotify API, takes a string representing the id and
 `   returns a dictionary including the keys 'followers', 'genres', 
     'id', 'name', and 'popularity'.
     """
-    pass
+    url = 'https://api.spotify.com/v1/artists/' + artist_id
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+    dct = req.json()
+    artist_info = {}
+    assert dct.get('name'), 'Artist not found.'
+    artist_info['followers'] = dct['followers']['total']
+    artist_info['genres'] = dct['genres']
+    artist_info['id'] = artist_id
+    artist_info['name'] = dct['name']
+    artist_info['popularity'] = dct['popularity']
+    return artist_info
 
