ebac87b5ec543e774aa5d00aa289b452a482da64
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-02-27 01:34:35 -0600

    Started working on final

---
+++ final_project/twitter_api.py
@@ -0,0 +1,9 @@
+import json, tweepy
+
+# keys saved separately in gitignored file for security reasons
+with open('auth.json', 'r') as f:
+    saved = json.load(f)
+consumer_key = saved['consumer_key']
+consumer_secret = saved['consumer_secret']
+access_token = saved['access_token']
+access_token_secret = saved['access_token_secret']
